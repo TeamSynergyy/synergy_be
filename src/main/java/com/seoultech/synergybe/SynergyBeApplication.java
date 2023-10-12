f0a5a52055c637aab52912618932cdb832152c13
jonghun <jonghuncu@gmail.com>
2023-10-12 20:43:41 +0900

    init project

---
+++ src/main/java/com/seoultech/synergybe/SynergyBeApplication.java
@@ -0,0 +1,13 @@
+package com.seoultech.synergybe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynergyBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SynergyBeApplication.class, args);
+    }
+
+}
