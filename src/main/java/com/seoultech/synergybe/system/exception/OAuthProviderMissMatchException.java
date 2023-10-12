f5e08b7e72260cfd2b52c69432fbd566f2ef377e
jonghun <jonghuncu@gmail.com>
2023-10-13 00:24:21 +0900

    feat : OAuth Social Login

---
+++ src/main/java/com/seoultech/synergybe/system/exception/OAuthProviderMissMatchException.java
@@ -0,0 +1,8 @@
+package com.seoultech.synergybe.system.exception;
+
+public class OAuthProviderMissMatchException extends RuntimeException {
+
+    public OAuthProviderMissMatchException(String message) {
+        super(message);
+    }
+}
