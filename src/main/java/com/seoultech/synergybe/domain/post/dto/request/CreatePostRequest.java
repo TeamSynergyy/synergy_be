32948afc41d45b9ba139a8470653ffa82e496e9a
jonghun <jonghuncu@gmail.com>
2023-10-19 01:17:11 +0900

    feat : Post Request, Response DTO

---
+++ src/main/java/com/seoultech/synergybe/domain/post/dto/request/CreatePostRequest.java
@@ -0,0 +1,21 @@
+package com.seoultech.synergybe.domain.post.dto.request;
+
+import com.seoultech.synergybe.domain.post.Post;
+import com.seoultech.synergybe.domain.user.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class CreatePostRequest {
+    private String title;
+    private String content;
+
+    public Post toEntity(User user) {
+        return Post.builder()
+                .user(user)
+                .title(title)
+                .content(content)
+                .build();
+    }
+}
---
+++ src/main/java/com/seoultech/synergybe/domain/post/dto/response/PostResponse.java
@@ -0,0 +1,47 @@
+package com.seoultech.synergybe.domain.post.dto.response;
+
+import com.seoultech.synergybe.domain.comment.Comment;
+import com.seoultech.synergybe.domain.post.Post;
+import com.seoultech.synergybe.domain.user.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.data.domain.Page;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@AllArgsConstructor
+@Getter
+@Builder
+public class PostResponse {
+    private Long postId;
+    private String title;
+    private String content;
+    private String authorName;
+    private String authorId;
+    private int likes;
+    private List<Comment> comments;
+    private LocalDateTime createAt;
+    private LocalDateTime updateAt;
+
+    public static PostResponse from(Post post) {
+        return new PostResponse(post.getId(), post.getTitle(), post.getContent(), post.getUser().getUsername(), post.getUser().getUserId(),
+                post.getLikes().size(), post.getComments(), post.getCreateAt(), post.getUpdateAt());
+    }
+
+    public static Page<PostResponse> from(Page<Post> posts) {
+        return posts.map(post -> PostResponse.builder()
+                .postId(post.getId())
+                .title(post.getTitle())
+                .content(post.getContent())
+                .authorName(post.getUser().getUsername())
+                .authorId(post.getUser().getUserId())
+                .likes(post.getComments().size())
+                .comments(post.getComments())
+                .createAt(post.getCreateAt())
+                .updateAt(post.getUpdateAt())
+                .build()
+        );
+    }
+}
