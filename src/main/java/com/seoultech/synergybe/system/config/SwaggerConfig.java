1528c85666499eb3f8cc41798ef3486b897d1e75
jonghun <jonghuncu@gmail.com>
2023-10-26 20:11:53 +0900

    feat : SwaggerConfig 수정

--- src/main/java/com/seoultech/synergybe/system/config/SwaggerConfig.java
+++ src/main/java/com/seoultech/synergybe/system/config/SwaggerConfig.java
@@ -1,46 +1,24 @@
 package com.seoultech.synergybe.system.config;
 
-import org.springframework.beans.factory.annotation.Value;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2
 public class SwaggerConfig {
-
-    @Value("${swagger.base-package}")
-    private String API_BASE_PACKAGE;
-
-    @Value("${swagger.path}")
-    private String API_PATH;
-
-    @Value("${swagger.version}")
-    private String API_VERSION;
-
     @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.OAS_30)
-                .useDefaultResponseMessages(false)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage(API_BASE_PACKAGE))
-                .paths(PathSelectors.ant(API_PATH))
-                .build()
-                .apiInfo(apiInfo())
-                ;
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
     }
 
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("Synergy backend API Test with Swagger")
-                .description("Synergy Swagger")
-                .version(API_VERSION)
-                .build();
+    private Info apiInfo() {
+        return new Info()
+                .title("Springdoc 테스트")
+                .description("Springdoc을 사용한 Swagger UI 테스트")
+                .version("1.0.0");
     }
 }
