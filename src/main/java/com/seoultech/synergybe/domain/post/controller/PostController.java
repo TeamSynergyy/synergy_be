a9a3363ac9fa0932ed1443704aa778ba3173a340
jonghun <jonghuncu@gmail.com>
2023-11-06 12:13:21 +0900

    fix : controller 설명 추가

--- src/main/java/com/seoultech/synergybe/domain/post/controller/PostController.java
+++ src/main/java/com/seoultech/synergybe/domain/post/controller/PostController.java
@@ -25,7 +25,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(value = "/api/v1/posts")
-@Tag(name = "게시물 api", description = "게시글을 CRUD하는 api 입니다")
+@Tag(name = "게시글 api", description = "게시글 관련 api 입니다")
 public class PostController {
     private final PostService postService;
 
@@ -37,8 +37,9 @@ public class PostController {
     }
 
 
-    @Operation(summary = "post 생성", description = "post를 생성하며 이미지를 받을 수 있습니다.")
+    @Operation(summary = "post 생성", description = "PostResponse가 반환되며 이미지가 함께 저장됩니다.")
     @PostMapping
+//    @PreAuthorize("hasRole('ROLE_USER')")
     public ResponseEntity<ApiResponse<PostResponse>> createPost(@ModelAttribute CreatePostRequest request) {
         org.springframework.security.core.userdetails.User principal = (org.springframework.security.core.userdetails.User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
@@ -47,24 +48,27 @@ public class PostController {
         return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.create("post", postService.createPost(user, request)));
     }
 
+    @Operation(summary = "post 수정", description = "PostResponse가 반환되며 UpdatePostRequest에 담긴 내용으로 수정됩니다.")
     @PutMapping
     public ResponseEntity<ApiResponse<PostResponse>> updatePost(@RequestBody UpdatePostRequest request) {
         org.springframework.security.core.userdetails.User principal = (org.springframework.security.core.userdetails.User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
         User user = userService.getUser(principal.getUsername());
 
-        return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success("post", postService.updatePost(user, request)));
+        return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success("post", postService.updatePost(request)));
     }
 
+    @Operation(summary = "post 삭제", description = "DeletePostResponse가 반환되며, post의 isDelete = true 로 값이 변경됩니다")
     @DeleteMapping(value = "/{postId}")
     public ResponseEntity<ApiResponse<DeletePostResponse>> deletePost(@PathVariable("postId") Long postId) {
         org.springframework.security.core.userdetails.User principal = (org.springframework.security.core.userdetails.User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
         User user = userService.getUser(principal.getUsername());
 
-        return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success("post", postService.deletePost(user, postId)));
+        return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success("post", postService.deletePost(postId)));
     }
 
+    @Operation(summary = "단건 Post Get", description = "요청된 1개의 Post가 반환됩니다")
     @GetMapping(value = "/{postId}")
     public ResponseEntity<ApiResponse<PostResponse>> getPost(@PathVariable("postId") Long postId) {
         org.springframework.security.core.userdetails.User principal = (org.springframework.security.core.userdetails.User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
@@ -73,12 +77,15 @@ public class PostController {
 
         return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success("post", postService.getPost(user, postId)));
     }
+
+    @Operation(summary = "최근 Post 리스트", description = "ListPostResponse가 반환되며, end값으로 마지막 조회된 post의 개수를 전달받으며 이후 10개의 post만 반환합니다")
     @GetMapping(value = "/recent")
     public ResponseEntity<ApiResponse<ListPostResponse>> getPosts(@RequestParam(value = "end", required = false, defaultValue = "9223372036854775807") Long end) {
 
         return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success("post list", postService.getPostList(end)));
     }
 
+    @Operation(summary = "Feed Post Get", description = "요청된 1개의 Post가 반환됩니다")
     @GetMapping(value = "/feed")
     public ResponseEntity<ApiResponse<ListPostResponse>> getFeed(@RequestParam(value = "end", required = false, defaultValue = "9223372036854775807") Long end) {
         org.springframework.security.core.userdetails.User principal = (org.springframework.security.core.userdetails.User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
@@ -126,6 +133,6 @@ public class PostController {
 
         User user = userService.getUser(principal.getUsername());
 
-        return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success("week best posts", postService.getWeekBestPostList(user)));
+        return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success("week best posts", postService.getWeekBestPostList()));
     }
 }
