b75dd1fad57f080e613ba2cadf1a48d1ef760005
jonghun <jonghuncu@gmail.com>
2023-11-11 04:50:05 +0900

    feat : 유저 response 추가

--- src/main/java/com/seoultech/synergybe/domain/user/dto/response/UserResponse.java
+++ src/main/java/com/seoultech/synergybe/domain/user/dto/response/UserResponse.java
@@ -6,6 +6,9 @@ import lombok.Builder;
 import lombok.Getter;
 import org.springframework.data.domain.Page;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @AllArgsConstructor
 @Getter
 @Builder
@@ -18,10 +21,15 @@ public class UserResponse {
     private String major;
     private String temperature;
     private String bio;
+    private String minor;
+    private String interestAreas;
+    private String skills;
+
 
     public static UserResponse from(User user) {
         return new UserResponse(user.getUserId(), user.getUsername(), user.getEmail(), user.getProfileImageUrl(),
-                "", "", "", "");
+                "", user.getMajor(), user.getTemperature(), user.getBio(), user.getMinor(), user.getInterestAreas(),
+                user.getSkills());
     }
 
     public static Page<UserResponse> from(Page<User> users) {
@@ -32,7 +40,32 @@ public class UserResponse {
                 .major(user.getMajor())
                 .temperature(user.getTemperature())
                 .bio(user.getBio())
+                .minor(user.getMinor())
+                .interestAreas(user.getInterestAreas())
+                .skills(user.getSkills())
                 .build()
         );
+    }
+
+    public static List<UserResponse> from(List<User> users) {
+        return users.stream()
+                .map(user -> UserResponse.builder()
+                        .userId(user.getUserId())
+                        .username(user.getUsername())
+                        .email(user.getEmail())
+                        .profileImageUrl(user.getProfileImageUrl())
+                        .major(user.getMajor())
+                        .temperature(user.getTemperature())
+                        .bio(user.getBio())
+                        .minor(user.getMinor())
+                        .interestAreas(user.getInterestAreas())
+                        .skills(user.getSkills())
+                        .build()
+                ).collect(Collectors.toList());
+    }
+
+    public static List<UserResponse> fromEmpty(List<User> users) {
+        return users.stream()
+                .map(user -> UserResponse.builder().build()).collect(Collectors.toList());
     }
 }
