b6c5d514f1e5d8f2fcfdcb1cfe8444b38f565d3e
jonghun <jonghuncu@gmail.com>
2023-11-27 19:38:11 +0900

    fix : project Apiresponse 제거

--- src/main/java/com/seoultech/synergybe/domain/project/controller/ProjectController.java
+++ src/main/java/com/seoultech/synergybe/domain/project/controller/ProjectController.java
@@ -7,7 +7,6 @@ import com.seoultech.synergybe.domain.project.dto.response.ProjectResponse;
 import com.seoultech.synergybe.domain.project.service.ProjectService;
 import com.seoultech.synergybe.domain.user.User;
 import com.seoultech.synergybe.domain.user.service.UserService;
-import com.seoultech.synergybe.system.common.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -16,8 +15,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api/v1/projects")
@@ -28,67 +25,67 @@ public class ProjectController {
     private final UserService userService;
 
     @PostMapping
-    public ResponseEntity<ApiResponse<ProjectResponse>> createProject(@RequestBody CreateProjectRequest request) {
+    public ResponseEntity<ProjectResponse> createProject(@RequestBody CreateProjectRequest request) {
         org.springframework.security.core.userdetails.User principal = (org.springframework.security.core.userdetails.User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
         User user = userService.getUser(principal.getUsername());
 
-        return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.create("project create", projectService.createProject(user, request)));
+        return ResponseEntity.status(HttpStatus.CREATED).body(projectService.createProject(user, request));
     }
 
     @PutMapping
-    public ResponseEntity<ApiResponse<ProjectResponse>> updateProject(@RequestBody UpdateProjectRequest request) {
+    public ResponseEntity<ProjectResponse> updateProject(@RequestBody UpdateProjectRequest request) {
         org.springframework.security.core.userdetails.User principal = (org.springframework.security.core.userdetails.User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
         User user = userService.getUser(principal.getUsername());
 
-        return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success("project update", projectService.updateProject(user, request)));
+        return ResponseEntity.status(HttpStatus.OK).body(projectService.updateProject(user, request));
     }
 
     @DeleteMapping(value = "/{projectId}")
-    public ResponseEntity<ApiResponse<ProjectResponse>> deleteProject(@PathVariable("projectId") Long projectId) {
+    public ResponseEntity<ProjectResponse> deleteProject(@PathVariable("projectId") Long projectId) {
 
-        return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success("project delete", projectService.deleteProject(projectId)));
+        return ResponseEntity.status(HttpStatus.OK).body(projectService.deleteProject(projectId));
     }
 
     @GetMapping("/{projectId}")
-    public ResponseEntity<ApiResponse<ProjectResponse>> getProject(@PathVariable("projectId") Long projectId) {
+    public ResponseEntity<ProjectResponse> getProject(@PathVariable("projectId") Long projectId) {
 
-        return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success("project get", projectService.getProject(projectId)));
+        return ResponseEntity.ok().body(projectService.getProject(projectId));
     }
 
     @GetMapping("/recent")
-    public ResponseEntity<ApiResponse<ListProjectResponse>> getProjectList(@RequestParam(value = "end", required = false, defaultValue = "9223372036854775807") Long end) {
+    public ResponseEntity<ListProjectResponse> getProjectList(@RequestParam(value = "end", required = false, defaultValue = "9223372036854775807") Long end) {
 
-        return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success("project list", projectService.getProjectList(end)));
+        return ResponseEntity.status(HttpStatus.OK).body(projectService.getProjectList(end));
     }
 
     @GetMapping
-    public ResponseEntity<ApiResponse<Page<ProjectResponse>>> searchAllProjects(@RequestParam("search") String search, @PageableDefault(size = 7) Pageable pageable) {
+    public ResponseEntity<Page<ProjectResponse>> searchAllProjects(@RequestParam("search") String search, @PageableDefault(size = 7) Pageable pageable) {
 
-        return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success("project search result", projectService.searchAllProjects(search, pageable)));
+        return ResponseEntity.status(HttpStatus.OK).body(projectService.searchAllProjects(search, pageable));
     }
 
     @GetMapping(value = "/me/likes")
-    public ResponseEntity<ApiResponse<ListProjectResponse>> getLikedProjects() {
+    public ResponseEntity<ListProjectResponse> getLikedProjects() {
         org.springframework.security.core.userdetails.User principal = (org.springframework.security.core.userdetails.User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
         User user = userService.getUser(principal.getUsername());
 
-        return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success("liked project list", projectService.getLikedProjectList(user)));
+        return ResponseEntity.status(HttpStatus.OK).body(projectService.getLikedProjectList(user));
     }
 
     @GetMapping(value = "/other")
-    public ResponseEntity<ApiResponse<ListProjectResponse>> getProjectsByUser(@RequestParam("userId") String userId) {
-        return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success("project list by user", projectService.getProjectListByUser(userId)));
+    public ResponseEntity<ListProjectResponse> getProjectsByUser(@RequestParam("userId") String userId) {
+        return ResponseEntity.status(HttpStatus.OK).body(projectService.getProjectListByUser(userId));
     }
 
     @GetMapping(value = "/recommend")
-    public ResponseEntity<ApiResponse<ListProjectResponse>> getRecommendProjects(@RequestParam(value = "end", required = false, defaultValue = "0") Long end) {
+    public ResponseEntity<ListProjectResponse> getRecommendProjects(@RequestParam(value = "end", required = false, defaultValue = "0") Long end) {
         org.springframework.security.core.userdetails.User principal = (org.springframework.security.core.userdetails.User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
         User user = userService.getUser(principal.getUsername());
 
-        return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success("recommend projects", projectService.getRecommendListByUser(user, end)));
+        return ResponseEntity.status(HttpStatus.OK).body(projectService.getRecommendListByUser(user, end));
     }
 }
