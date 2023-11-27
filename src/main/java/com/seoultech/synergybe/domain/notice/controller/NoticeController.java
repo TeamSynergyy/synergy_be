18c61dd15ee93d46ab7ab1ef9c473b51be20af89
jonghun <jonghuncu@gmail.com>
2023-11-27 19:43:45 +0900

    fix : notice Apiresponse 제거

--- src/main/java/com/seoultech/synergybe/domain/notice/controller/NoticeController.java
+++ src/main/java/com/seoultech/synergybe/domain/notice/controller/NoticeController.java
@@ -3,7 +3,6 @@ package com.seoultech.synergybe.domain.notice.controller;
 import com.seoultech.synergybe.domain.notice.dto.request.NoticeRequest;
 import com.seoultech.synergybe.domain.notice.dto.response.NoticeResponse;
 import com.seoultech.synergybe.domain.notice.service.NoticeService;
-import com.seoultech.synergybe.system.common.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,21 +17,21 @@ public class NoticeController {
     private final NoticeService noticeService;
 
     @PostMapping
-    public ResponseEntity<ApiResponse<NoticeResponse>> createNotice(@RequestBody NoticeRequest request) {
+    public ResponseEntity<NoticeResponse> createNotice(@RequestBody NoticeRequest request) {
 
-        return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.create("notice", this.noticeService.createNotice(request)));
+        return ResponseEntity.status(HttpStatus.CREATED).body(noticeService.createNotice(request));
     }
 
     @GetMapping(value = "/{projectId}")
-    public ResponseEntity<ApiResponse<List<NoticeResponse>>> getNotice(@PathVariable("projectId") Long projectId) {
+    public ResponseEntity<List<NoticeResponse>> getNotice(@PathVariable("projectId") Long projectId) {
 
-        return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success("notice list", this.noticeService.getNoticeList(projectId)));
+        return ResponseEntity.status(HttpStatus.OK).body(noticeService.getNoticeList(projectId));
     }
 
     @DeleteMapping("/{noticeId}")
-    public ResponseEntity<ApiResponse<NoticeResponse>> deleteNotice(@PathVariable("noticeId") Long noticeId) {
+    public ResponseEntity<NoticeResponse> deleteNotice(@PathVariable("noticeId") Long noticeId) {
 
-        return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success("notice delete", this.noticeService.deleteNotice(noticeId)));
+        return ResponseEntity.status(HttpStatus.OK).body(noticeService.deleteNotice(noticeId));
     }
 
 }
