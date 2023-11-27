004dde1747b73d0f7c2380c9f2644026d7e4e241
jonghun <jonghuncu@gmail.com>
2023-11-27 19:46:56 +0900

    fix : comment Apiresponse 제거

--- src/main/java/com/seoultech/synergybe/domain/comment/controller/CommentController.java
+++ src/main/java/com/seoultech/synergybe/domain/comment/controller/CommentController.java
@@ -5,7 +5,6 @@ import com.seoultech.synergybe.domain.comment.dto.response.CommentResponse;
 import com.seoultech.synergybe.domain.comment.service.CommentService;
 import com.seoultech.synergybe.domain.user.User;
 import com.seoultech.synergybe.domain.user.service.UserService;
-import com.seoultech.synergybe.system.common.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,28 +21,28 @@ public class CommentController {
     private final UserService userService;
 
     @PostMapping
-    public ResponseEntity<ApiResponse<CommentResponse>> createComment(@RequestBody CommentRequest request) {
+    public ResponseEntity<CommentResponse> createComment(@RequestBody CommentRequest request) {
         org.springframework.security.core.userdetails.User principal = (org.springframework.security.core.userdetails.User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
         User user = userService.getUser(principal.getUsername());
 
-        return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.create("comment create", commentService.createComment(user, request)));
+        return ResponseEntity.status(HttpStatus.CREATED).body(commentService.createComment(user, request));
     }
 
     @PutMapping
-    public ResponseEntity<ApiResponse<CommentResponse>> updateComment(@RequestBody CommentRequest request) {
+    public ResponseEntity<CommentResponse> updateComment(@RequestBody CommentRequest request) {
 
-        return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success("comment update", commentService.updateComment(request)));
+        return ResponseEntity.status(HttpStatus.OK).body(commentService.updateComment(request));
     }
 
     @DeleteMapping
-    public ResponseEntity<ApiResponse<CommentResponse>> deleteComment(@RequestBody CommentRequest request) {
+    public ResponseEntity<CommentResponse> deleteComment(@RequestBody CommentRequest request) {
 
-        return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success("comment delete", commentService.deleteComment(request)));
+        return ResponseEntity.status(HttpStatus.OK).body(commentService.deleteComment(request));
     }
 
     @GetMapping(value = "/{postId}")
-    public ResponseEntity<ApiResponse<List<CommentResponse>>> getCommentList(@PathVariable("postId") Long postId) {
-        return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success("comment list from post", commentService.getCommentList(postId)));
+    public ResponseEntity<List<CommentResponse>> getCommentList(@PathVariable("postId") Long postId) {
+        return ResponseEntity.status(HttpStatus.OK).body(commentService.getCommentList(postId));
     }
 }
