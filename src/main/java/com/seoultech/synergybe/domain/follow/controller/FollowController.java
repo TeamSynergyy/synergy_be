4709c1a46afaf7e933708ca8c4c5b71e3583c7ca
jonghun <jonghuncu@gmail.com>
2023-11-27 19:46:47 +0900

    fix : follow Apiresponse 제거

--- src/main/java/com/seoultech/synergybe/domain/follow/controller/FollowController.java
+++ src/main/java/com/seoultech/synergybe/domain/follow/controller/FollowController.java
@@ -5,7 +5,6 @@ import com.seoultech.synergybe.domain.follow.dto.response.FollowResponse;
 import com.seoultech.synergybe.domain.follow.service.FollowService;
 import com.seoultech.synergybe.domain.user.User;
 import com.seoultech.synergybe.domain.user.service.UserService;
-import com.seoultech.synergybe.system.common.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,12 +20,12 @@ public class FollowController {
     private final UserService userService;
 
     @PutMapping(value = "/{followingId}")
-    public ResponseEntity<ApiResponse<FollowResponse>> updateFollow(@PathVariable("followingId") String followingId, @RequestBody FollowType type) {
+    public ResponseEntity<FollowResponse> updateFollow(@PathVariable("followingId") String followingId, @RequestBody FollowType type) {
         org.springframework.security.core.userdetails.User principal = (org.springframework.security.core.userdetails.User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
         User user = userService.getUser(principal.getUsername());
 
-        return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success("follow", followService.updateFollow(user, followingId,type)));
+        return ResponseEntity.status(HttpStatus.OK).body(followService.updateFollow(user, followingId,type));
 
     }
 
