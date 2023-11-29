a70134ceb09ad0f89dc07240c071de9caad67206
jonghun <jonghuncu@gmail.com>
2023-11-29 22:33:03 +0900

    feat : failResponse dto 생성

---
+++ src/main/java/com/seoultech/synergybe/system/exception/dto/NotFoundFailResponse.java
@@ -0,0 +1,17 @@
+package com.seoultech.synergybe.system.exception.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class NotFoundFailResponse {
+    private final int status;
+
+    private final String message;
+
+    @Builder
+    public NotFoundFailResponse(int status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+}
