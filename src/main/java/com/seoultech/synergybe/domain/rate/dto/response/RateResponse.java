1044e3a8b1e6b020aa8c9314c49310aa57d7b844
jonghun <jonghuncu@gmail.com>
2023-11-30 18:28:52 +0900

    feat : dto에서 rateList 반환

--- src/main/java/com/seoultech/synergybe/domain/rate/dto/response/RateResponse.java
+++ src/main/java/com/seoultech/synergybe/domain/rate/dto/response/RateResponse.java
@@ -5,14 +5,33 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @AllArgsConstructor
 @Getter
+@Builder
 public class RateResponse {
     private Long projectId;
+    private Long rateId;
+    private String giveUserId;
+    private String receiveUserId;
     private int score;
 
-    @Builder
     public static RateResponse from(Rate savedRate) {
-        return new RateResponse(savedRate.getProject().getId(), savedRate.getScore());
+        return new RateResponse(savedRate.getProject().getId(), savedRate.getId(), savedRate.getGiveUser().getUserId(),
+                savedRate.getReceiveUser().getUserId(), savedRate.getScore());
+    }
+
+    public static List<RateResponse> from(List<Rate> rates) {
+        return rates.stream()
+                .map(rate -> RateResponse.builder()
+                        .projectId(rate.getProject().getId())
+                        .rateId(rate.getId())
+                        .giveUserId(rate.getGiveUser().getUserId())
+                        .receiveUserId(rate.getReceiveUser().getUserId())
+                        .score(rate.getScore())
+                        .build())
+                .collect(Collectors.toList());
     }
 }
