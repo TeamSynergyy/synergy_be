435e44e167e0b08c18bcfa2ab5d16fa6f390479d
jonghun <jonghuncu@gmail.com>
2023-11-30 17:00:42 +0900

    fix : query 수정

--- src/main/java/com/seoultech/synergybe/domain/ticket/repository/TicketRepository.java
+++ src/main/java/com/seoultech/synergybe/domain/ticket/repository/TicketRepository.java
@@ -10,6 +10,6 @@ import java.util.List;
 
 @Repository
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
-    @Query(value = "SELECT * FROM ticket WHERE project_id =: projectId", nativeQuery = true)
+    @Query(value = "SELECT * FROM ticket WHERE project_id = :projectId", nativeQuery = true)
     List<Ticket> findAllByProjectId(@Param("projectId") Long projectId);
 }
