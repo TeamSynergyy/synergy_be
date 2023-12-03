3475fc0db839f01fa5de49b5fccc6af9a3a04558
jonghun <jonghuncu@gmail.com>
2023-12-03 21:27:19 +0900

    feat : 구독 controller 생성

--- src/main/java/com/seoultech/synergybe/domain/notification/controller/NotificationController.java
+++ src/main/java/com/seoultech/synergybe/domain/notification/controller/NotificationController.java
@@ -1,13 +1,19 @@
 package com.seoultech.synergybe.domain.notification.controller;
 
 import com.seoultech.synergybe.domain.notification.service.NotificationService;
+import com.seoultech.synergybe.system.config.login.LoginUser;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping
 @RequiredArgsConstructor
 public class NotificationController {
-    private NotificationService notificationService;
+    private final NotificationService notificationService;
+
+    @GetMapping(value = "/subscribe", produces = "text/event-stream")
+    public SseEmitter subscribe(@LoginUser String userId, @RequestHeader(value = "Last-Event-ID", required = false, defaultValue = "") String lastEventId) {
+        return notificationService.subscribe(userId, lastEventId);
+    }
 }
