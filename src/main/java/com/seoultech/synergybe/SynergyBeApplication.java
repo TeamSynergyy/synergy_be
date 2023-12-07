60c0ef482fedb17ea0b34a3ca4c2d7022b1baf65
jonghun <jonghuncu@gmail.com>
2023-12-07 22:47:05 +0900

    feat : caching 어노테이션 추가

--- src/main/java/com/seoultech/synergybe/SynergyBeApplication.java
+++ src/main/java/com/seoultech/synergybe/SynergyBeApplication.java
@@ -3,9 +3,11 @@ package com.seoultech.synergybe;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableCaching
 @EnableJpaAuditing
 @ConfigurationPropertiesScan("com.seoultech.synergybe.system.config.properties")
 public class SynergyBeApplication {
