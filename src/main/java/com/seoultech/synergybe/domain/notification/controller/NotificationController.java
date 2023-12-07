d2d8fd46bbe8f8434adce6f770c707f1d0d39177
jonghun <jonghuncu@gmail.com>
2023-12-07 21:39:24 +0900

    docs : swagger 설명

--- src/main/java/com/seoultech/synergybe/domain/notification/controller/NotificationController.java
+++ src/main/java/com/seoultech/synergybe/domain/notification/controller/NotificationController.java
@@ -2,16 +2,18 @@ package com.seoultech.synergybe.domain.notification.controller;
 
 import com.seoultech.synergybe.domain.notification.service.NotificationService;
 import com.seoultech.synergybe.system.config.login.LoginUser;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @RestController
-@RequestMapping
+@RequestMapping(value = "/api/v1")
 @RequiredArgsConstructor
 public class NotificationController {
     private final NotificationService notificationService;
 
+    @Operation(summary = "알림 구독", description = "userId에 대해 알림을 구독합니다.")
     @GetMapping(value = "/subscribe", produces = "text/event-stream")
     public SseEmitter subscribe(@LoginUser String userId, @RequestHeader(value = "Last-Event-ID", required = false, defaultValue = "") String lastEventId) {
         return notificationService.subscribe(userId, lastEventId);
