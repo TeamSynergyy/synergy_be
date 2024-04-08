5d811587e60dce60dd8ebd374166061aef2a6b06
rivkode <jonghuncu@gmail.com>
2024-04-08 17:46:17 +0900

    #55 feat : UserDetailsService 인터페이스 구현

---
+++ src/main/java/com/seoultech/synergybe/system/security/UserDetailsServiceImpl.java
@@ -0,0 +1,24 @@
+package com.seoultech.synergybe.system.security;
+
+import com.seoultech.synergybe.domain.user.User;
+import com.seoultech.synergybe.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Not found" + email));
+        return new UserDetailsImpl(user);
+    }
+}
