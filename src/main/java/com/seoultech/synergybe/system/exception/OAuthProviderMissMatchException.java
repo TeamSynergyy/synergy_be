0369f70f53b096aa3659f0363e2c07254aa3b4ac
rivkode <jonghuncu@gmail.com>
2024-04-09 14:36:13 +0900

    #55 remove : Oauth 기능 삭제

--- src/main/java/com/seoultech/synergybe/system/exception/OAuthProviderMissMatchException.java
+++
@@ -1,8 +0,0 @@
-package com.seoultech.synergybe.system.exception;
-
-public class OAuthProviderMissMatchException extends RuntimeException {
-
-    public OAuthProviderMissMatchException(String message) {
-        super(message);
-    }
-}
