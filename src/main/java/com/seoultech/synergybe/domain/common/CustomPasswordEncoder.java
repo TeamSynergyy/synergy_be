5ba1c631593d05a9481001c6fcef1cc6b90d599a
rivkode <jonghuncu@gmail.com>
2024-04-12 09:52:45 +0900

    #61 feat : CustomPasswordEncoder 생성

---
+++ src/main/java/com/seoultech/synergybe/domain/common/CustomPasswordEncoder.java
@@ -0,0 +1,7 @@
+package com.seoultech.synergybe.domain.common;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class CustomPasswordEncoder extends BCryptPasswordEncoder {
+
+}
