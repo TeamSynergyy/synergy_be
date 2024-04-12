637c4931dee8e87e6cd3e2b6460a6baf2bf7218e
rivkode <jonghuncu@gmail.com>
2024-04-12 13:54:47 +0900

    #64 feat : RuntimeException, NotFoundException 예외 처리

--- src/main/java/com/seoultech/synergybe/system/exception/GlobalExceptionHandler.java
+++ src/main/java/com/seoultech/synergybe/system/exception/GlobalExceptionHandler.java
@@ -4,7 +4,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.*;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -13,4 +13,19 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(BAD_REQUEST)
                 .body(new ErrorResponse(e.getErrorCode(), e.getMessage()));
     }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ErrorResponse> runtimeEx(RuntimeException e) {
+        return ResponseEntity.status(INTERNAL_SERVER_ERROR)
+                .body(new ErrorResponse(ErrorCode.INTERNAL_SERVER_ERROR.getValue(),
+                        "예측하지 못한 예외가 발생하였습니다." + e.getMessage()));
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<ErrorResponse> notFoundEx(NotFoundException e) {
+        return ResponseEntity.status(NOT_FOUND)
+                .body(new ErrorResponse(e.getErrorCode(), e.getMessage()));
+    }
+
+
 }
