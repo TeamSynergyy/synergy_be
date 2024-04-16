3ad448b11847999847cf03f89993782477b38c1c
rivkode <jonghuncu@gmail.com>
2024-04-16 19:43:55 +0900

    #72 fix : ProjectInformation VO 생성

--- src/main/java/com/seoultech/synergybe/domain/project/Project.java
+++ src/main/java/com/seoultech/synergybe/domain/project/Project.java
@@ -1,8 +1,11 @@
 package com.seoultech.synergybe.domain.project;
 
 import com.seoultech.synergybe.domain.apply.Apply;
+import com.seoultech.synergybe.domain.common.entity.IsDeleted;
 import com.seoultech.synergybe.domain.notice.Notice;
 import com.seoultech.synergybe.domain.project.dto.request.UpdateProjectRequest;
+import com.seoultech.synergybe.domain.project.vo.ProjectInformation;
+import com.seoultech.synergybe.domain.project.vo.ProjectLocation;
 import com.seoultech.synergybe.domain.projectlike.ProjectLike;
 import com.seoultech.synergybe.domain.projectuser.ProjectUser;
 import com.seoultech.synergybe.domain.schedule.Schedule;
@@ -20,6 +23,8 @@ import jakarta.persistence.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+
+import static com.seoultech.synergybe.domain.common.constants.DeletedStatus.IS_DELETED_DEFAULT;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
@@ -41,19 +46,17 @@ public class Project extends BaseTime {
     @Enumerated(EnumType.STRING)
     private ProjectStatus status;
 
-    private LocalDateTime startAt;
+    @Embedded
+    private ProjectInformation information;
 
-    private LocalDateTime endAt;
-
-    private String leaderId;
-
-    private Point location;
+    @Embedded
+    private ProjectLocation location;
 
     @OneToMany(mappedBy = "project")
     private List<Notice> notices = new ArrayList<>();
 
-    @Column(name = "is_deleted")
-    private boolean isDeleted;
+    @Embedded
+    private IsDeleted isDeleted = new IsDeleted(IS_DELETED_DEFAULT);
 
     @OneToMany(
             mappedBy = "project"
@@ -80,21 +83,15 @@ public class Project extends BaseTime {
         this.name = name;
         this.content = content;
         this.field = field;
-        this.location = location;
+        this.location = new ProjectLocation(location);
         this.status = ProjectStatus.READY;
-        this.startAt = startAt;
-        this.endAt = endAt;
-        this.leaderId = leaderId;
-        this.isDeleted = false;
+        this.information = new ProjectInformation(startAt, endAt, leaderId);
     }
 
     public Project updateProject(UpdateProjectRequest request) {
         this.name = request.getName();
         this.content = request.getContent();
         this.field = request.getField();
-        this.location = request.getLocation();
-        this.startAt = request.getStartAt();
-        this.endAt = request.getEndAt();
 
         return this;
     }
