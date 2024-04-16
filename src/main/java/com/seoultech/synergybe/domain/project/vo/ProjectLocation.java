85b091e864b5a36c956fc3a656d57c3ad2359431
rivkode <jonghuncu@gmail.com>
2024-04-16 19:44:20 +0900

    #72 feat : ProjectLocation VO 생성

---
+++ src/main/java/com/seoultech/synergybe/domain/project/vo/ProjectLocation.java
@@ -0,0 +1,36 @@
+package com.seoultech.synergybe.domain.project.vo;
+
+import com.seoultech.synergybe.domain.project.exception.ProjectBadRequestException;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.geo.Point;
+
+import java.util.Objects;
+
+@Getter
+@Embeddable
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ProjectLocation {
+
+    @Column(name = "location")
+    private Point location;
+
+    public ProjectLocation(Point value) {
+        validateNotNull(location);
+        this.location = value;
+    }
+
+    private void validateNotNull(Point value) {
+        if (Objects.isNull(value)) {
+            throw new ProjectBadRequestException("위치 정보는 필수 항목입니다.");
+        }
+    }
+
+    public void updateLocation(Point value) {
+        validateNotNull(value);
+        this.location = value;
+    }
+}
