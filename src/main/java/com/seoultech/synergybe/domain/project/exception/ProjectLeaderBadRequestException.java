1205c73b6e0df0d957fe031b9d4d3d8ab6d090dc
rivkode <jonghuncu@gmail.com>
2024-04-16 19:43:31 +0900

    #72 rename : exception 문자 추가

--- src/main/java/com/seoultech/synergybe/domain/project/exception/ProjectLeaderBadRequestException.java
+++ src/main/java/com/seoultech/synergybe/domain/project/exception/ProjectLeaderBadRequestException.java
@@ -4,8 +4,8 @@ import com.seoultech.synergybe.system.exception.BadRequestException;
 
 import static com.seoultech.synergybe.system.exception.ErrorCode.BAD_REQUEST;
 
-public class ProjectLeaderBadRequest extends BadRequestException {
-    public ProjectLeaderBadRequest(String message) {
+public class ProjectLeaderBadRequestException extends BadRequestException {
+    public ProjectLeaderBadRequestException(String message) {
         super(BAD_REQUEST, message);
     }
 }
