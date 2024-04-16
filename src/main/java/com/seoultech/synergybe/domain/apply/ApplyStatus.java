49497d83dae70f8af6c79a2138053d4a243a2514
rivkode <jonghuncu@gmail.com>
2024-04-16 18:52:59 +0900

    #72 fix : applyStatus -> EnumType 구현

--- src/main/java/com/seoultech/synergybe/domain/apply/ApplyStatus.java
+++ src/main/java/com/seoultech/synergybe/domain/apply/ApplyStatus.java
@@ -1,5 +1,16 @@
 package com.seoultech.synergybe.domain.apply;
 
-public enum ApplyStatus {
-    PROCESS, REJECTED, COMPLETED
+import com.seoultech.synergybe.domain.common.EnumType;
+
+public enum ApplyStatus implements EnumType {
+    NEW,
+    REJECTED,
+    ACCEPT,
+    COMPLETED
+    ;
+
+    @Override
+    public String getName() {
+        return this.name();
+    }
 }
