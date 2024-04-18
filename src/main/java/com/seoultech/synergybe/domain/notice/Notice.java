8a7d78f83592dc2f0435a2f478943d996da34690
rivkode <jonghuncu@gmail.com>
2024-04-18 18:14:03 +0900

    #72 fix : Notice VO 생성, 적용

--- src/main/java/com/seoultech/synergybe/domain/notice/Notice.java
+++ src/main/java/com/seoultech/synergybe/domain/notice/Notice.java
@@ -1,6 +1,9 @@
 package com.seoultech.synergybe.domain.notice;
 
 
+import com.seoultech.synergybe.domain.common.entity.IsDeleted;
+import com.seoultech.synergybe.domain.notice.vo.NoticeContent;
+import com.seoultech.synergybe.domain.notice.vo.NoticeTitle;
 import com.seoultech.synergybe.domain.project.Project;
 import com.seoultech.synergybe.domain.common.BaseTime;
 import lombok.AccessLevel;
@@ -12,6 +15,8 @@ import org.hibernate.annotations.Where;
 
 import jakarta.persistence.*;
 
+import static com.seoultech.synergybe.domain.common.constants.DeletedStatus.IS_DELETED_DEFAULT;
+
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -22,22 +27,25 @@ public class Notice extends BaseTime {
     @Column(name = "notice_id")
     private String id;
 
-    @Column(columnDefinition = "TEXT")
-    private String content;
+    @Embedded
+    private NoticeTitle title;
+
+    @Embedded
+    private NoticeContent content;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "project_id")
     private Project project;
 
-    @Column(name = "is_deleted")
-    private boolean isDeleted;
+    @Embedded
+    private IsDeleted isDeleted = new IsDeleted(IS_DELETED_DEFAULT);
 
 
     @Builder
-    public Notice(String id, String content, Project project) {
+    public Notice(String id, String title, String content, Project project) {
         this.id = id;
-        this.content = content;
+        this.title = new NoticeTitle(title);
+        this.content = new NoticeContent(content);
         this.project = project;
-        this.isDeleted = false;
     }
 }
