3ef906f24356969e9c948db922895894775a70bd
rivkode <jonghuncu@gmail.com>
2024-04-23 13:04:42 +0900

    #78 fix : user dto class -> record 변경

--- src/main/java/com/seoultech/synergybe/domain/user/dto/request/UpdateUserRequest.java
+++ src/main/java/com/seoultech/synergybe/domain/user/dto/request/UpdateUserRequest.java
@@ -1,16 +1,14 @@
 package com.seoultech.synergybe.domain.user.dto.request;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 
-@AllArgsConstructor
-@Getter
-public class UpdateUserRequest {
-    private String username;
-    private String bio;
-    private String major;
-    private String minor;
-    private String interestAreas;
-    private String skills;
-    private String organization;
+import jakarta.validation.constraints.NotBlank;
+
+public record UpdateUserRequest(
+        @NotBlank(message = "이메일은 필수 항목 입니다.")
+        String email,
+        @NotBlank(message = "이름은 필수 항목 입니다.")
+        String name,
+        @NotBlank(message = "전공은 필수 항목 입니다.")
+        String major
+) {
 }
