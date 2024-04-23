905dc86246d5c5566d907c17bf9852af681d02b5
rivkode <jonghuncu@gmail.com>
2024-04-23 14:03:15 +0900

    #78 fix : project DTO class -> record 변경

--- src/main/java/com/seoultech/synergybe/domain/project/dto/request/CreateProjectRequest.java
+++ src/main/java/com/seoultech/synergybe/domain/project/dto/request/CreateProjectRequest.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.seoultech.synergybe.domain.project.Project;
 import com.seoultech.synergybe.domain.project.ProjectField;
 import com.seoultech.synergybe.domain.user.User;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -11,39 +12,41 @@ import org.springframework.data.geo.Point;
 
 import java.time.LocalDateTime;
 
-@AllArgsConstructor
-@Getter
-public class CreateProjectRequest {
-    private String name;
 
-    private String content;
+public record CreateProjectRequest(
+        @NotBlank(message = "이름은 필수항목입니다.")
+        String name,
+        @NotBlank(message = "내용은 필수항목입니다.")
+        String content,
+        @NotBlank(message = "분야는 필수항목입니다.")
+        ProjectField field,
+        @NotBlank(message = "경도은 필수항목입니다.")
+        Double longitude,
+        @NotBlank(message = "위도는 필수항목입니다.")
+        Double latitude,
+        @NotBlank(message = "시작일시는 필수항목입니다.")
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyy hh:mm:ss.SSS", timezone = "Asia/Seoul")
+        LocalDateTime startAt,
+        @NotBlank(message = "종료일시는 필수항목입니다.")
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyy hh:mm:ss.SSS", timezone = "Asia/Seoul")
+        LocalDateTime endAt
 
-    private ProjectField field;
+) {
 
-    private Double longitude;
-
-    private Double latitude;
-
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyy hh:mm:ss.SSS", timezone = "Asia/Seoul")
-    private LocalDateTime startAt;
-
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyy hh:mm:ss.SSS", timezone = "Asia/Seoul")
-    private LocalDateTime endAt;
-
-    public Project toEntity(User user) {
-        Point point = new Point(longitude, latitude);
-        try {
-            return Project.builder()
-                    .name(name)
-                    .content(content)
-                    .field(field)
-                    .location(point)
-                    .startAt(startAt)
-                    .endAt(endAt)
-                    .leaderId(user.getUserId())
-                    .build();
-        } catch (Exception e) {
-            throw new IllegalArgumentException("point parse exception");
-        }
-    }
+//    public Project toEntity(User user) {
+//        Point point = new Point(longitude, latitude);
+//        try {
+//            return Project.builder()
+//                    .name(name)
+//                    .content(content)
+//                    .field(field)
+//                    .location(point)
+//                    .startAt(startAt)
+//                    .endAt(endAt)
+//                    .leaderId(user.getUserId())
+//                    .build();
+//        } catch (Exception e) {
+//            throw new IllegalArgumentException("point parse exception");
+//        }
+//    }
 }
--- src/main/java/com/seoultech/synergybe/domain/project/dto/request/UpdateProjectRequest.java
+++ src/main/java/com/seoultech/synergybe/domain/project/dto/request/UpdateProjectRequest.java
@@ -4,45 +4,46 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.seoultech.synergybe.domain.project.Project;
 import com.seoultech.synergybe.domain.project.ProjectField;
 import com.seoultech.synergybe.domain.user.User;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.data.geo.Point;
 
 import java.time.LocalDateTime;
 
-@AllArgsConstructor
-@Getter
-public class UpdateProjectRequest {
+public record UpdateProjectRequest(
+        @NotBlank(message = "프로젝트 ID는 필수항목입니다.")
+        String projectId,
+        @NotBlank(message = "이름은 필수항목입니다.")
+        String name,
+        @NotBlank(message = "내용은 필수항목입니다.")
+        String content,
+        @NotBlank(message = "분야는 필수항목입니다.")
+        ProjectField field,
+        @NotBlank(message = "경도은 필수항목입니다.")
+        Double longitude,
+        @NotBlank(message = "위도는 필수항목입니다.")
+        Double latitude,
+        @NotBlank(message = "시작일시는 필수항목입니다.")
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyy hh:mm:ss.SSS", timezone = "Asia/Seoul")
+        LocalDateTime startAt,
+        @NotBlank(message = "종료일시는 필수항목입니다.")
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyy hh:mm:ss.SSS", timezone = "Asia/Seoul")
+        LocalDateTime endAt
+) {
 
-    private Long projectId;
-    private String name;
-
-    private String content;
-
-    private ProjectField field;
-
-    private Double longitude;
-
-    private Double latitude;
-
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyy hh:mm:ss.SSS", timezone = "Asia/Seoul")
-    private LocalDateTime startAt;
-
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyy hh:mm:ss.SSS", timezone = "Asia/Seoul")
-    private LocalDateTime endAt;
-
-    public Project toEntity(User user) {
-        return Project.builder()
-                .name(name)
-                .content(content)
-                .field(field)
-                .startAt(startAt)
-                .endAt(endAt)
-                .leaderId(user.getUserId())
-                .build();
-    }
-
-    public Point getLocation() {
-        return new Point(this.longitude, this.latitude);
-    }
+//    public Project toEntity(User user) {
+//        return Project.builder()
+//                .name(name)
+//                .content(content)
+//                .field(field)
+//                .startAt(startAt)
+//                .endAt(endAt)
+//                .leaderId(user.getUserId())
+//                .build();
+//    }
+//
+//    public Point getLocation() {
+//        return new Point(this.longitude, this.latitude);
+//    }
 }
