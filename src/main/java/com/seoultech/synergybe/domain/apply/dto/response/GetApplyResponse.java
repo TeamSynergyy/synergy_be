5738005c1398d40a6292abd82edcb86f6cbd46b7
rivkode <jonghuncu@gmail.com>
2024-04-25 08:53:26 +0900

    #82 fix : response 타입 ApplyStats -> String 변경

--- src/main/java/com/seoultech/synergybe/domain/apply/dto/response/GetApplyResponse.java
+++ src/main/java/com/seoultech/synergybe/domain/apply/dto/response/GetApplyResponse.java
@@ -2,11 +2,16 @@ package com.seoultech.synergybe.domain.apply.dto.response;
 
 import com.querydsl.core.annotations.QueryProjection;
 import com.seoultech.synergybe.domain.apply.ApplyStatus;
-import lombok.Builder;
 
-@Builder
 public record GetApplyResponse(
         String applyId,
-        ApplyStatus status
+        String status
 ) {
+    @QueryProjection
+    public GetApplyResponse(String applyId, ApplyStatus status) {
+        this(
+                applyId,
+                status.getName()
+        );
+    }
 }
