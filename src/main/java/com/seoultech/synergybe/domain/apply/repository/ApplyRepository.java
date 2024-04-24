ab362d9a74bc31497d5af4f9d20c74f1dc0dcff6
rivkode <jonghuncu@gmail.com>
2024-04-25 08:51:56 +0900

    #82 remove : 메서드 제거

--- src/main/java/com/seoultech/synergybe/domain/apply/repository/ApplyRepository.java
+++ src/main/java/com/seoultech/synergybe/domain/apply/repository/ApplyRepository.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface ApplyRepository extends JpaRepository<Apply, Long>, ApplyRepositoryCustom {
+public interface ApplyRepository extends JpaRepository<Apply, String>, ApplyRepositoryCustom {
 
 //    @Query(value = "SELECT * FROM apply WHERE user_id = :userId AND project_id = :projectId", nativeQuery = true)
 //    Optional<Apply> findByUserIdAndProjectId(@Param("userId") String userId, @Param("projectId") Long projectId);
--- src/main/java/com/seoultech/synergybe/domain/apply/repository/ApplyRepositoryCustom.java
+++ src/main/java/com/seoultech/synergybe/domain/apply/repository/ApplyRepositoryCustom.java
@@ -1,17 +1,14 @@
 package com.seoultech.synergybe.domain.apply.repository;
 
 import com.seoultech.synergybe.domain.apply.Apply;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface ApplyRepositoryCustom {
-    Optional<Apply> findApplyByUserIdAndProjectId(String userId, String projectId);
+    Apply findApplyByUserIdAndProjectId(String userId, String projectId);
 
     List<Apply> findAllProcessByUserId(String userId);
 
     List<String> findUserIdsByProjectId(String projectId);
-
-    List<Long> findProjectIdsByUserId(String userId);
 }
