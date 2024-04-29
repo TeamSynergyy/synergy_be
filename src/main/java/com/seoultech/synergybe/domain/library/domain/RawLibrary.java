717686200014628f976bfa0d5caefa635ad2e75e
rivkode <jonghuncu@gmail.com>
2024-04-29 17:23:24 +0900

    #79 feat : RawLibrary 엔티티 설계

---
+++ src/main/java/com/seoultech/synergybe/domain/library/domain/RawLibrary.java
@@ -0,0 +1,80 @@
+package com.seoultech.synergybe.domain.library.domain;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class RawLibrary {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "library_seq") // 도서관 일련번호
+    private String librarySeq;
+
+    @Column(name = "name") // 도서관 명
+    private String name;
+
+    @Column(name = "gu_code") // 구 코드
+    private String guCode;
+
+    @Column(name = "gu_code_value") // 구 명
+    private String guCodeValue;
+
+    @Column(name = "address") // 주소
+    private String address;
+
+    @Column(name = "tel_number") // 전화번호
+    private String telNumber;
+
+    @Column(name = "hompage_url") // 홈페이지 url
+    private String hompageUrl;
+
+    @Column(name = "op_time") // 운영시간
+    private String opTime;
+
+    @Column(name = "close_date") // 정기 휴관일
+    private String closeDate;
+
+    @Column(name = "se_name") // 도서관 구분명
+    private String seName;
+
+    @Column(name = "latitude") // 위도
+    private Double latitude;
+
+    @Column(name = "longitude") // 경도
+    private Double longitude;
+
+    @Builder
+    public RawLibrary(
+            String librarySeq,
+            String name,
+            String guCode,
+            String guCodeValue,
+            String address,
+            String telNumber,
+            String hompageUrl,
+            String opTime,
+            String closeDate,
+            String seName,
+            Double latitude,
+            Double longitude
+    ) {
+        this.librarySeq = librarySeq;
+        this.name = name;
+        this.guCode = guCode;
+        this.guCodeValue = guCodeValue;
+        this.address = address;
+        this.telNumber = telNumber;
+        this.hompageUrl = hompageUrl;
+        this.opTime = opTime;
+        this.closeDate = closeDate;
+        this.seName = seName;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+}
