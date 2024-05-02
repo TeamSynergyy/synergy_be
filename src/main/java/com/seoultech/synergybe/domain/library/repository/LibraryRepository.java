029fec9c8cef6d242ceb49efa057aa514e3f693f
rivkode <jonghuncu@gmail.com>
2024-05-02 11:19:27 +0900

    #79 remove : RawLibraryRepository 제거

--- src/main/java/com/seoultech/synergybe/domain/library/repository/LibraryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.seoultech.synergybe.domain.library.repository;
-
-import com.seoultech.synergybe.domain.library.domain.Library;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LibraryRepository extends JpaRepository<Library, String> {
-}
--- src/main/java/com/seoultech/synergybe/domain/library/repository/RawLibraryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.seoultech.synergybe.domain.library.repository;
-
-import com.seoultech.synergybe.domain.library.domain.RawLibrary;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RawLibraryRepository extends JpaRepository<RawLibrary, Long> {
-}
