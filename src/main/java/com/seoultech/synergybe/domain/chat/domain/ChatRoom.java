7e3410331e3477459c5c77a9480cd8dd6bdb718c
rivkode <jonghuncu@gmail.com>
2024-05-03 17:24:23 +0900

    #94 feat : ChatRoom 엔티티 생성

---
+++ src/main/java/com/seoultech/synergybe/domain/chat/domain/ChatRoom.java
@@ -0,0 +1,32 @@
+package com.seoultech.synergybe.domain.chat.domain;
+
+import com.seoultech.synergybe.domain.user.User;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class ChatRoom {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "sender_id", nullable = false)
+    private User sender;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "receiver_id", nullable = false)
+    private User receiver;
+
+
+    @Builder
+    public ChatRoom(User sender, User receiver) {
+        this.sender = sender;
+        this.receiver = receiver;
+    }
+
+}
