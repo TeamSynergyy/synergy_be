10c4d77eeaf70d63ad926c4d15cf093bbaabdbc5
rivkode <jonghuncu@gmail.com>
2024-05-08 16:45:08 +0900

    #94 feat : HashMap Key 를 위한 getKeyFromSession() 구현

--- src/main/java/com/seoultech/synergybe/domain/chat/handler/WebSocketSessionMap.java
+++ src/main/java/com/seoultech/synergybe/domain/chat/handler/WebSocketSessionMap.java
@@ -2,16 +2,28 @@ package com.seoultech.synergybe.domain.chat.handler;
 
 import lombok.Getter;
 import org.springframework.stereotype.Component;
+import org.springframework.web.socket.WebSocketSession;
 
 
 import java.util.HashMap;
+import java.util.Map;
 
 @Getter
 @Component
 public class WebSocketSessionMap {
-    private HashMap<Long, WebSocketSessionList> websocketListHashMap = new HashMap<>();
+    private final HashMap<Long, WebSocketSessionList> websocketListHashMap;
 
-    public void createWebSocketSessionList(Long chatRoomId, WebSocketSessionList webSocketSessionList) {
-        websocketListHashMap.put(chatRoomId, webSocketSessionList);
+    public WebSocketSessionMap() {
+        this.websocketListHashMap = new HashMap<>();
+    }
+
+    // 세션을 이용하여 HashMap의 키 값을 가져오는 메서드
+    public Long getKeyFromSession(WebSocketSession session) {
+        for (Map.Entry<Long, WebSocketSessionList> entry : websocketListHashMap.entrySet()) {
+            if (entry.getValue().contains(session)) {
+                return entry.getKey();
+            }
+        }
+        return null; // 세션을 찾지 못한 경우
     }
 }
