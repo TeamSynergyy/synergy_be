642094af8727ddbcd0bbae26443f95c09a0ec11b
rivkode <jonghuncu@gmail.com>
2024-05-08 16:50:10 +0900

    #94 fix : builder 패턴을 클래스 레벨 -> 메서드 레벌 이동

--- src/main/java/com/seoultech/synergybe/domain/chat/domain/ChatMessage.java
+++ src/main/java/com/seoultech/synergybe/domain/chat/domain/ChatMessage.java
@@ -1,22 +1,20 @@
 package com.seoultech.synergybe.domain.chat.domain;
 
-import com.seoultech.synergybe.domain.user.User;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToOne;
 import lombok.Builder;
 import lombok.Getter;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.time.LocalDateTime;
 
 @Document(collection = "chat_message")
 @Getter
-@Builder
 public class ChatMessage {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @Field("_id")
+    private String id;
 
     @Indexed
     private Long chatRoomId;
@@ -30,4 +28,17 @@ public class ChatMessage {
     private String imageName;
 
     private String imageUrl;
+    private LocalDateTime createAt;
+
+    @Builder
+    public ChatMessage(Long chatRoomId, String message, String userId, ChatType chatType, String imageName, String imageUrl,
+                       LocalDateTime createAt) {
+        this.chatRoomId = chatRoomId;
+        this.message = message;
+        this.userId = userId;
+        this.chatType = chatType;
+        this.imageName = imageName;
+        this.imageUrl = imageUrl;
+        this.createAt = createAt;
+    }
 }
