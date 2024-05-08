0b0a9c99eaccaa5c3dcd743a727d9f7a84d76b40
rivkode <jonghuncu@gmail.com>
2024-05-08 17:12:46 +0900

    #94 feat : 작은도서관 스케줄러 생성

---
+++ src/main/java/com/seoultech/synergybe/domain/library/scheduler/RawSmallLibraryScheduler.java
@@ -0,0 +1,13 @@
+package com.seoultech.synergybe.domain.library.scheduler;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+@RequiredArgsConstructor
+@Transactional
+@Slf4j
+public class RawSmallLibraryScheduler {
+}
---
+++ src/main/java/com/seoultech/synergybe/domain/library/scheduler/SmallLibraryScheduler.java
@@ -0,0 +1,4 @@
+package com.seoultech.synergybe.domain.library.scheduler;
+
+public class SmallLibraryScheduler {
+}
