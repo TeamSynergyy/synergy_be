1c3b64e466618953a8eeaab916da3716321cd778
rivkode <jonghuncu@gmail.com>
2024-05-08 16:48:47 +0900

    #94 remove : 사용하지 않는 로직 제거

--- src/main/java/com/seoultech/synergybe/domain/chat/service/ChatRoomService.java
+++ src/main/java/com/seoultech/synergybe/domain/chat/service/ChatRoomService.java
@@ -2,17 +2,13 @@ package com.seoultech.synergybe.domain.chat.service;
 
 import com.seoultech.synergybe.domain.chat.domain.ChatRoom;
 import com.seoultech.synergybe.domain.chat.dto.request.CreateChatRoomRequest;
-import com.seoultech.synergybe.domain.chat.handler.ChatHandler;
-import com.seoultech.synergybe.domain.chat.handler.WebSocketSessionList;
-import com.seoultech.synergybe.domain.chat.handler.WebSocketSessionMap;
-import com.seoultech.synergybe.domain.chat.repository.ChatRoomRepository;
+import com.seoultech.synergybe.domain.chat.dto.response.GetChatRoomResponse;
+import com.seoultech.synergybe.domain.chat.jpa_repository.ChatRoomRepository;
 import com.seoultech.synergybe.domain.user.User;
 import com.seoultech.synergybe.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.web.socket.WebSocketSession;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -20,9 +16,6 @@ import java.util.List;
 public class ChatRoomService {
     private final UserService userService;
     private final ChatRoomRepository chatRoomRepository;
-    private final WebSocketSessionMap webSocketSessionMap;
-    private final ChatHandler chatHandler;
-
     public void createRoom(CreateChatRoomRequest request) {
         User createUser = userService.getUser(request.createUserId());
         User attendUser = userService.getUser(request.attendUserId());
@@ -33,21 +26,19 @@ public class ChatRoomService {
                 .name(request.roomName())
                 .build();
 
-        // todo
-        // session 생성
-
-        List<WebSocketSession> webSocketSessions = new ArrayList<>();
-        WebSocketSessionList webSocketSessionList = WebSocketSessionList.builder()
-                .webSocketSessions(webSocketSessions)
-                .build();
-
-        Long chatRoomId = chatRoom.getId();
-//        webSocketSessionMap.createWebSocketSessionList(chatRoomId, webSocketSessionList);
-
-        // session createUser 연결
-//        chatHandler.afterConnectionEstablished();
-
 
         chatRoomRepository.save(chatRoom);
+    }
+
+    public List<GetChatRoomResponse> getChatRoomsByUserId(String userId) {
+        List<ChatRoom> chatRooms = chatRoomRepository.findAllByCreateUserIdOrAttendUserId(userId);
+
+        List<GetChatRoomResponse> getChatRoomResponses = chatRooms.stream().map(
+                chatRoom -> new GetChatRoomResponse(
+                        chatRoom.getId(), chatRoom.getName()
+                )
+        ).toList();
+
+        return getChatRoomResponses;
     }
 }
