6962e80c0bf607cb7ad0f9519f8f09a7df56270f
rivkode <jonghuncu@gmail.com>
2024-05-08 16:46:19 +0900

    #94 feat : chat Controller, Repository 생성

---
+++ src/main/java/com/seoultech/synergybe/domain/chat/controller/ChatMessageController.java
@@ -0,0 +1,21 @@
+package com.seoultech.synergybe.domain.chat.controller;
+
+import com.seoultech.synergybe.domain.chat.domain.ChatMessage;
+import com.seoultech.synergybe.domain.chat.service.ChatMessageService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/chat")
+@RequiredArgsConstructor
+public class ChatMessageController {
+    private final ChatMessageService chatMessageService;
+
+    @GetMapping("/{chatRoomId}")
+    public List<ChatMessage> getChatList(@PathVariable("chatRoomId") Long chatRoomId) {
+
+        return chatMessageService.getChatListByChatRoomId(chatRoomId);
+    }
+}
