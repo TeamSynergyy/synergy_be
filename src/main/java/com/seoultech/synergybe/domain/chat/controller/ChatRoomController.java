398712c78cfd70b2fa93c228e5e3c2aaf40610f1
rivkode <jonghuncu@gmail.com>
2024-05-08 16:49:05 +0900

    #94 feat : Get 채팅방 리스트

--- src/main/java/com/seoultech/synergybe/domain/chat/controller/ChatRoomController.java
+++ src/main/java/com/seoultech/synergybe/domain/chat/controller/ChatRoomController.java
@@ -1,18 +1,21 @@
 package com.seoultech.synergybe.domain.chat.controller;
 
+import com.seoultech.synergybe.domain.chat.domain.ChatRoom;
 import com.seoultech.synergybe.domain.chat.dto.request.CreateChatRoomRequest;
+import com.seoultech.synergybe.domain.chat.dto.response.GetChatRoomResponse;
 import com.seoultech.synergybe.domain.chat.service.ChatRoomService;
+import com.seoultech.synergybe.system.config.login.LoginUser;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/rooms")
+@RequestMapping("/api/v1/rooms")
 public class ChatRoomController {
     private final ChatRoomService chatRoomService;
 
@@ -21,7 +24,11 @@ public class ChatRoomController {
         chatRoomService.createRoom(request);
 
         return ResponseEntity.noContent().build();
+    }
 
+    @GetMapping
+    public ResponseEntity<List<GetChatRoomResponse>> getChatRoomList(@LoginUser String userId) {
 
+        return ResponseEntity.status(HttpStatus.OK).body(chatRoomService.getChatRoomsByUserId(userId));
     }
 }
