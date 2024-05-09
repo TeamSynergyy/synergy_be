92c54784df024a8cd1094c027f23cf8bbe0b14fc
rivkode <jonghuncu@gmail.com>
2024-05-09 10:10:23 +0900

    #94 fix : 유저 검증 추가 / 채팅 전송시 저장로직 책임 분리

--- src/main/java/com/seoultech/synergybe/domain/chat/handler/ChatHandler.java
+++ src/main/java/com/seoultech/synergybe/domain/chat/handler/ChatHandler.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.seoultech.synergybe.domain.chat.domain.ChatType;
 import com.seoultech.synergybe.domain.chat.dto.request.ChatMessageRequest;
 import com.seoultech.synergybe.domain.chat.service.ChatMessageService;
+import com.seoultech.synergybe.domain.user.User;
+import com.seoultech.synergybe.domain.user.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
@@ -20,35 +22,42 @@ import java.util.Map;
 @Component
 //@RequiredArgsConstructor
 public class ChatHandler extends TextWebSocketHandler {
-//    private static List<WebSocketSession> webSocketSessions = new ArrayList<>();
     private WebSocketSessionMap webSocketSessionMap; // 채팅방별 세션리스트 모음
     private ObjectMapper objectMapper;
     private ChatMessageService chatMessageService;
+    private UserService userService;
 
 
 
     // 채팅 핸들러 생성
-    public ChatHandler(ObjectMapper objectMapper, ChatMessageService chatMessageService) {
+    public ChatHandler(ObjectMapper objectMapper, ChatMessageService chatMessageService, UserService userService) {
         this.webSocketSessionMap = new WebSocketSessionMap();
         this.objectMapper = objectMapper;
         this.chatMessageService = chatMessageService;
+        this.userService = userService;
     }
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-//        webSocketSessions.add(session);
-        Long chatRoomId = webSocketSessionMap.getKeyFromSession(session);
-
-        // 여기서 request를 어떻게 알지 ?
-        if (chatRoomId == null) {
-            // 채팅방 생성
-//            chatRoomService.createRoom();
-        }
-
-
-//        log.info("session add : " + session.getId());
     }
 
+    // todo
+    // 좀 더 책임을 나누자
+
+    /**
+     * 전체 로직
+     * - TextMessage에서 payLoad를 가져옴
+     * - payLoad 값을 통해 objectMapper를 사용하여 ChatMessageRequest Dto 로 변환
+     * - payLoad에 담긴 userId를 통해 user 검증
+     * - payLoad에 담긴 chatRoomId를 통해 websocketList 탐색
+     *   - 만약 없으면 생성
+     * - chatType이 ENTER일 경우 session 에 add
+     * - chatType이 TEXT일 경우 sendMessage(), saveMessage() 호출
+     * - websocket connection을 close할 경우 해당 session을 websocket List에서 remove
+     * @param session
+     * @param message
+     * @throws Exception
+     */
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
         String payLoad = message.getPayload();
@@ -57,6 +66,10 @@ public class ChatHandler extends TextWebSocketHandler {
         // 채팅 Dto로 변환
         ChatMessageRequest chatMessageRequest = objectMapper.readValue(payLoad, ChatMessageRequest.class);
         log.info("session {}", chatMessageRequest.toString());
+
+        // 유저 검증
+        userService.getUser(chatMessageRequest.userId());
+
 
         // payload에 chatroomId 가져옴
         Long chatRoomId = chatMessageRequest.chatRoomId();
@@ -80,18 +93,27 @@ public class ChatHandler extends TextWebSocketHandler {
 
         // 만약 입장하는 경우라면 (Type이 Enter 라면)
         if (chatMessageRequest.chatType().equals(ChatType.ENTER)) {
+            // afterConnectionEstablished -> 이 메소들 사용해서 session add는 안되는지 다시 고민
 //            afterConnectionEstablished(session);
+            log.info("before add session List size : {}", webSocketSessionList.getWebSocketSessions().size());
             // 현재 들어온 세션을 해당 채팅방 세션리스트에 추가
             webSocketSessionList.getWebSocketSessions().add(session);
-            log.info("session add / session Id : {}", session.getId());
+            log.info("session add | session Id : {}", session.getId());
         }
 
         // 만약 텍스트를 보낸다면
         if (chatMessageRequest.chatType().equals(ChatType.TEXT)) {
-            // 채팅 전송
-            sendAndSaveMessageToChatRoom(chatMessageRequest, webSocketSessionList);
-        }
+            log.info("websocket Session List size : {}",webSocketSessionList.getWebSocketSessions().size());
 
+            // 채팅 전송
+            sendMessageToChatRoom(chatMessageRequest, webSocketSessionList);
+
+            // 한사람에 대해서만 저장을 해야함
+            saveMessage(chatMessageRequest);
+        } else if (chatMessageRequest.chatType().equals(ChatType.IMAGE)) {
+            // todo
+            // 이미지 혹은 영상 처리
+        }
     }
 
 
@@ -99,8 +121,6 @@ public class ChatHandler extends TextWebSocketHandler {
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
         Long chatRoomId = webSocketSessionMap.getKeyFromSession(session);
-
-
 
         // 얘가 호출되니까 여기서 removeClosedSession을 호출해야지
         // 그런데 roomId로 해당하는 채팅방의 채팅세션리스트들을 가져와야하는데 ?
@@ -110,21 +130,18 @@ public class ChatHandler extends TextWebSocketHandler {
         List<WebSocketSession> webSocketSessions = getSessionListByChatRoomId(chatRoomId);
         webSocketSessions.remove(session);
 
-        log.info("session remove / session Id : {}", session.getId());
+        log.info("session remove | session Id : {}", session.getId());
+        log.info("after connection closed session List size : {}",webSocketSessions.size());
     }
 
     private List<WebSocketSession> getSessionListByChatRoomId(Long chatRoomId) {
         return webSocketSessionMap.getWebsocketListHashMap().get(chatRoomId).getWebSocketSessions();
     }
 
-    private void sendAndSaveMessageToChatRoom(ChatMessageRequest chatMessageRequest, WebSocketSessionList webSocketSessionList) {
+    private void sendMessageToChatRoom(ChatMessageRequest chatMessageRequest, WebSocketSessionList webSocketSessionList) {
         for (WebSocketSession session : webSocketSessionList.getWebSocketSessions()) {
             sendMessage(session, chatMessageRequest.message());
-            saveMessage(chatMessageRequest);
         }
-
-//        webSocketSessionList.getWebSocketSessions().parallelStream().forEach(sess -> sendMessage(sess, chatMessageRequest.message()));
-
     }
 
     private void saveMessage(ChatMessageRequest chatMessageRequest) {
