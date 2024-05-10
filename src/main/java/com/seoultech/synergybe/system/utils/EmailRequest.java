0fa5ad849d92e0a60198227e240eff9ac0e6d137
rivkode <jonghuncu@gmail.com>
2024-05-10 15:15:28 +0900

    #97 feat : email, 인증번호 request Dto record 로 생성

---
+++ src/main/java/com/seoultech/synergybe/system/utils/EmailRequest.java
@@ -0,0 +1,17 @@
+package com.seoultech.synergybe.system.utils;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+
+@Valid
+public record EmailRequest(
+        @NotEmpty(message = "이메일을 입력해주세요.")
+        @Email
+        String email,
+
+        @NotEmpty(message = "인증 번호를 입력해주세요.")
+        String authNumber
+) {
+
+}
