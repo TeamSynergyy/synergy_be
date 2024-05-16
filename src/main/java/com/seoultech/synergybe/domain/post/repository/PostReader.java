7e7f3de43e41531819f95dd014f5f5767e98333a
rivkode <jonghuncu@gmail.com>
2024-05-16 15:43:13 +0900

    #99 feat : read() 메서드 생성

--- src/main/java/com/seoultech/synergybe/domain/post/repository/PostReader.java
+++ src/main/java/com/seoultech/synergybe/domain/post/repository/PostReader.java
@@ -1,5 +1,22 @@
 package com.seoultech.synergybe.domain.post.repository;
 
-public interface PostReader extends PostRepository{
+import com.seoultech.synergybe.domain.post.Post;
+import com.seoultech.synergybe.domain.post.exception.PostNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class PostReader {
+    private final PostRepository postRepository;
+
+    public Post read(String postId) {
+        return postRepository.findById(postId)
+                .orElseThrow(() -> new PostNotFoundException("존재하지 않는 게시글입니다."));
+    }
+
+
+
+
 
 }
