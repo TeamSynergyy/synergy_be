20dbf6cd5f25d5686e942b75e21349d84986d098
rivkode <jonghuncu@gmail.com>
2024-05-16 15:43:49 +0900

    #99 feat : postValidator 검증 객체 생성

---
+++ src/main/java/com/seoultech/synergybe/domain/post/service/PostValidator.java
@@ -0,0 +1,15 @@
+package com.seoultech.synergybe.domain.post.service;
+
+import com.seoultech.synergybe.domain.post.Post;
+import com.seoultech.synergybe.domain.post.exception.PostBadRequestException;
+import com.seoultech.synergybe.domain.user.User;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PostValidator {
+    public void validateUser(User user, Post post) {
+        if (!post.getUser().equals(user)) {
+            throw new PostBadRequestException("인증되지 않은 유저입니다.");
+        }
+    }
+}
