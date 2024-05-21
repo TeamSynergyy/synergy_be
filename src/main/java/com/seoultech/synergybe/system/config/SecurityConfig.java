9d86ecd6b584a638acb5d58e4f6fcb61bcdb364b
rivkode <jonghuncu@gmail.com>
2024-05-21 16:54:31 +0900

    #100 feat : reader, factory 의존성 추가

--- src/main/java/com/seoultech/synergybe/system/config/SecurityConfig.java
+++ src/main/java/com/seoultech/synergybe/system/config/SecurityConfig.java
@@ -2,11 +2,12 @@ package com.seoultech.synergybe.system.config;
 
 import com.seoultech.synergybe.domain.auth.JwtAuthenticationProvider;
 import com.seoultech.synergybe.domain.common.CustomPasswordEncoder;
+import com.seoultech.synergybe.domain.user.repository.UserRefreshTokenFactory;
+import com.seoultech.synergybe.domain.user.service.UserRefreshTokenReader;
 import com.seoultech.synergybe.system.security.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -14,8 +15,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
@@ -31,6 +30,8 @@ import java.util.List;
 public class SecurityConfig {
 
     private final JwtUtil jwtUtil;
+    private final UserRefreshTokenReader userRefreshTokenReader;
+    private final UserRefreshTokenFactory userRefreshTokenFactory;
     private final UserDetailsServiceImpl userDetailsService;
     private final AuthenticationConfiguration authenticationConfiguration;
     private final JwtAuthenticationProvider authenticationProvider;
@@ -48,7 +49,7 @@ public class SecurityConfig {
     // Authenticatoin, 토큰에 대해 인증
     @Bean
     public JwtAuthenticationFilter jwtAuthenticationFilter() throws Exception {
-        JwtAuthenticationFilter filter = new JwtAuthenticationFilter(jwtUtil);
+        JwtAuthenticationFilter filter = new JwtAuthenticationFilter(jwtUtil, userRefreshTokenReader, userRefreshTokenFactory);
         filter.setAuthenticationManager(authenticationManager(authenticationConfiguration));
 
         return filter;
@@ -58,7 +59,7 @@ public class SecurityConfig {
     // Authorization, 식별된 사용자에 대해 권한 부여, 인가
     @Bean
     public JwtAuthorizationFilter jwtAuthorizationFilter() {
-        return new JwtAuthorizationFilter(jwtUtil, userDetailsService, authenticationProvider);
+        return new JwtAuthorizationFilter(jwtUtil, userDetailsService, authenticationProvider, userRefreshTokenReader);
     }
     @Bean
     public SecurityFilterChain securityFilterChain(
