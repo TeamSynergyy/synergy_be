069ed3c5c3498e4f7e242f88d8a1ea61e77c0a8c
rivkode <jonghuncu@gmail.com>
2024-05-21 16:55:33 +0900

    #100 feat : refreshToken update, valid 로직 추가

--- src/main/java/com/seoultech/synergybe/system/security/JwtAuthorizationFilter.java
+++ src/main/java/com/seoultech/synergybe/system/security/JwtAuthorizationFilter.java
@@ -2,20 +2,22 @@ package com.seoultech.synergybe.system.security;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.seoultech.synergybe.domain.auth.JwtAuthenticationProvider;
+import com.seoultech.synergybe.domain.user.UserRefreshToken;
+import com.seoultech.synergybe.domain.user.service.UserRefreshTokenReader;
 import com.seoultech.synergybe.system.apiresponse.ApiResponseDto;
 import io.jsonwebtoken.Claims;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -24,11 +26,13 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
     private final JwtUtil jwtUtil;
     private final UserDetailsServiceImpl userDetailsService;
     private final JwtAuthenticationProvider authenticationProvider;
+    private final UserRefreshTokenReader userRefreshTokenReader;
 
-    public JwtAuthorizationFilter(JwtUtil jwtUtil, UserDetailsServiceImpl userDetailsService, JwtAuthenticationProvider authenticationProvider) {
+    public JwtAuthorizationFilter(JwtUtil jwtUtil, UserDetailsServiceImpl userDetailsService, JwtAuthenticationProvider authenticationProvider, UserRefreshTokenReader userRefreshTokenReader) {
         this.jwtUtil = jwtUtil;
         this.userDetailsService = userDetailsService;
         this.authenticationProvider = authenticationProvider;
+        this.userRefreshTokenReader = userRefreshTokenReader;
     }
 
     @Override
@@ -39,7 +43,7 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
 
         if (StringUtils.hasText(tokenValue)) {
             // 토큰 검증
-            if (notValidate(response, tokenValue)) return;
+            if (isValidate(request, response, tokenValue)) return;
 
             // 토큰에서 사용자 정보 가져오기
             Claims info = getClaims(response, tokenValue);
@@ -52,34 +56,73 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
         filterChain.doFilter(request, response);
     }
 
-    private boolean notValidate(HttpServletResponse res, String tokenValue) throws IOException {
+    private boolean isValidate(HttpServletRequest request, HttpServletResponse response, String tokenValue) throws IOException {
+        // jwt 검증 진행 로직
         if (!jwtUtil.validateToken(tokenValue)) {
-            res.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-            res.setContentType("application/json");
+            // 만료되었을 경우
+            // refreshToken가 존재하면 accessToken 재발급, refreshToken 재발급
+
+            String refreshToken = getRefreshTokenFromRequest(request);
+
+            if (refreshToken.equals("expiration")) {
+                // 재로그인 요청
+                return true;
+            }
+            String userId = "";
+            String email = "";
+
+            // accessToken 재발급
+            String newRefreshToken = jwtUtil.createToken(userId, email);
+
+
+
+
+            // refreshToken 재발급
+            UserRefreshToken userRefreshToken = userRefreshTokenReader.readByRefreshToken(newRefreshToken);
+            userRefreshToken.updateRefreshToken();
+
+
+
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            response.setContentType("application/json");
             String result = new ObjectMapper().writeValueAsString(
                     new ApiResponseDto(HttpStatus.BAD_REQUEST.value(), "INVALID_TOKEN")
             );
 
-            res.getOutputStream().print(result);
+            response.getOutputStream().print(result);
             return true;
         }
         return false;
     }
 
-    private Claims getClaims(HttpServletResponse res, String tokenValue) throws IOException {
+    private String getRefreshTokenFromRequest(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie c : cookies) {
+                String name = c.getName();
+                String value = c.getValue();
+                if (name.equals("refreshToken")) {
+                    return value;
+                }
+            }
+        }
+        return "expiration";
+    }
+
+    private Claims getClaims(HttpServletResponse response, String tokenValue) throws IOException {
         Claims info;
 
         try {
             info = jwtUtil.getUserInfoFromToken(tokenValue);
         } catch (Exception e) {
             // JWT 검증에 실패한 경우 처리
-            res.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-            res.setContentType("application/json");
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            response.setContentType("application/json");
             String result = new ObjectMapper().writeValueAsString(
                     new ApiResponseDto(HttpStatus.BAD_REQUEST.value(), "INVALID_TOKEN")
             );
 
-            res.getOutputStream().print(result);
+            response.getOutputStream().print(result);
             return null;
         }
         return info;
@@ -115,5 +158,4 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
         String userId = userDetails.getUserId();
         return new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
     }
-
 }
