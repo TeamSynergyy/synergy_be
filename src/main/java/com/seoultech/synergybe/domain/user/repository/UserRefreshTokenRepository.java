a6039050141a15126d56956e4ae47380dea49ac3
rivkode <jonghuncu@gmail.com>
2024-05-21 16:52:55 +0900

    #100 feat : findByRefreshToken() 메서드 추가

--- src/main/java/com/seoultech/synergybe/domain/user/repository/UserRefreshTokenRepository.java
+++ src/main/java/com/seoultech/synergybe/domain/user/repository/UserRefreshTokenRepository.java
@@ -4,8 +4,12 @@ import com.seoultech.synergybe.domain.user.UserRefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRefreshTokenRepository extends JpaRepository<UserRefreshToken, Long> {
-    UserRefreshToken findByUserId(String userId);
+    Optional<UserRefreshToken> findByUserId(String userId);
     UserRefreshToken findByUserIdAndRefreshToken(String userId, String refreshToken);
+
+    UserRefreshToken findByRefreshToken(String token);
 }
