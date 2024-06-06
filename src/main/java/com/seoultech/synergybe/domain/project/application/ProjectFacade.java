b4c1bf314049a0d1f2979c283157eb2b03f983bd
jonghun <jonghuncu@gmail.com>
2024-06-06 10:35:45 +0900

    #107 feat: Project 애플리케이션 생성

--- src/main/java/com/seoultech/synergybe/domain/project/application/ProjectFacade.java
+++ src/main/java/com/seoultech/synergybe/domain/project/application/ProjectFacade.java
@@ -1,4 +1,17 @@
 package com.seoultech.synergybe.domain.project.application;
 
+import com.seoultech.synergybe.domain.project.domain.service.ProjectServiceV2;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
 public class ProjectFacade {
+    private final ProjectServiceV2 projectServiceV2;
+    public ProjectInfo registerProject(ProjectCommand command) {
+
+
+    }
 }
