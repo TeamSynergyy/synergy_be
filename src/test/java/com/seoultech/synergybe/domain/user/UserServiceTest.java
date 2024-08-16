971b7f22a9abc6f7c3798aea634d8c0d2f23f70a
jonghun <jonghuncu@gmail.com>
2024-08-16 22:55:34 +0900

    #115 test: createUser()

---
+++ src/test/java/com/seoultech/synergybe/domain/user/UserServiceTest.java
@@ -0,0 +1,40 @@
+package com.seoultech.synergybe.domain.user;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.seoultech.synergybe.domain.user.repository.UserRepository;
+import com.seoultech.synergybe.domain.user.service.UserService;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+public class UserServiceTest {
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @DisplayName("유저를 등록한다.")
+    @Test
+    void createUser() {
+        // given
+        String email = "jonghuncu@gmail.com";
+        String password = "password";
+        String name = "name";
+        String major = "major";
+
+        // when
+        String userToken = userService.createUser(email, password, name, major);
+        User user = userRepository.findByUserToken(userToken).orElseThrow();
+
+        // then
+        assertThat(user.getName().getName()).isEqualTo(name);
+        assertThat(user.getEmail().getEmail()).isEqualTo(email);
+    }
+}
